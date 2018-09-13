048ff7fa0752ec31e183ef9d950287f9a43630dc
saya <sa0726866@gmail.com>
2018-09-13 23:34:11 +0900

    some example for week 3

--- Assets/Scripts/MainCharaControl.cs
+++ Assets/Scripts/MainCharaControl.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class MainCharaControl : MonoBehaviour {
 
-    public enum MoveType
-    {
-        Velocity, 
-        Force, 
-        Transform, 
-    }
+	public enum MoveType
+	{
+		Velocity, 
+		Force, 
+		Transform, 
+	}
 
-    public MoveType MoveWay = MoveType.Velocity;
-    public float Speed = 100f;
+	public MoveType MoveWay = MoveType.Velocity;
+	public float Speed = 100f;
 
-    private Rigidbody2D rb;
+	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.left;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-        if (MoveWay == MoveType.Velocity)
-        {
-            rb.velocity = (new Vector2(hor, ver)) * Speed * Time.deltaTime;
-        }
-        else if (MoveWay == MoveType.Transform)
-        {
-            transform.Translate(new Vector2(hor, ver) * Speed * Time.deltaTime / 10);
-        }
-        else if (MoveWay == MoveType.Force)
-        {
-            rb.AddRelativeForce(new Vector2(hor, ver) * Speed * Time.deltaTime);
-            //rb.AddForce(new Vector2(hor, ver) * Speed * Time.deltaTime);
-        }
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		if (MoveWay == MoveType.Velocity)
+		{
+			rb.velocity = (new Vector2(x, y)) * Speed;
+		}
+		else if (MoveWay == MoveType.Transform)
+		{
+			transform.Translate(new Vector2(x, y) * Speed / 10);
+		}
+		else if (MoveWay == MoveType.Force)
+		{
+			rb.AddForce(new Vector2(x, y) * Speed);
+		}
 	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("collide!!");
-    }
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("collide!!");
+	}
 }
---
+++ Assets/Scripts/RightMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightMover : MonoBehaviour {
+
+	public int Integer = 8;
+	public float Float = .25f;
+	public bool Bool = true;
+	public string String = "Shxt";
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		Debug.Log(Integer);
+		Debug.Log(Float);
+		Debug.Log(Bool);
+		Debug.Log(String);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		float speedRate = 1f;
+		if (Input.GetKey(KeyCode.LeftControl))
+		{
+			speedRate = 2.5f;
+		}
+		rb.velocity = new Vector2(x, y) * speedRate;
+	}
+}
